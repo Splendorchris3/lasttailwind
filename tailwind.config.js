67a46afdad01b2b3ca1e008e37c9c8a86811219c
Splendid <ndubuisixtiangod@gmail.com>
2023-06-08 16:51:14 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+
+  colors: {
+    "regal-blue": "#243c5a",
+  },
+  plugins: [],
+};
